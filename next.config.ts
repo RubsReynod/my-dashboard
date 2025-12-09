34f7466121531be246ce98d979c62612a022d4ff
Rubs <ruben.23292.rr@gmail.com>
2025-12-09 13:40:39 -0600

    40 - Next image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("https://images.unsplash.com/photo-1542909168-82c3e7fdca5c"),
+    ],
+  },
 };
 
 export default nextConfig;
